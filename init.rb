0b0e21bee88e28917bbf34c274cafe6766482ef8
Sébastien Leroux <sleroux@keep.pt>
2012-11-19 18:26:36 +0000

    Updated plugin description

--- init.rb
+++ init.rb
@@ -5,10 +5,10 @@ require 'account_controller_patch'
 Redmine::Plugin.register :redmine_self_service_password do
   name 'Redmine Self Service Password plugin'
   author 'Sébastien Leroux'
-  description 'This is a plugin that allows Redmine to work with the Keep\'s Authentication Management Service'
+  description 'This is a plugin that allows Redmine to work with the Self Service Password application (http://ltb-project.org/wiki/documentation/self-service-password)'
   version '0.0.4'
   author_url 'mailto:sleroux@keep.pt'
-
+  url 'https://github.com/keeps/redmine_self_service_password.git'
   settings :default => {
     'selfServicePasswordURL' => 'https://www.keep.pt/ams/',
     'selfServicePasswordName' => 'KEEP SOLUTIONS authentication management service'
